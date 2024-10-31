25433a8aff930e4565c285c5baf93a0632aaefa3
DonaldGubin <bassboosted111@mail.ru>
2024-10-31 21:40:20 +0300

    'last'

--- Lab2/LrBr2/LrBr2/Temp/Archives/SampleText/4PP065_0571_P74/libDriveLib.c
+++
@@ -1,6 +0,0 @@
-void DriveStateMachine(void) {};
-void _DriveStateMachine(void) {};
-void DoorStateMachine(void) {};
-void _DoorStateMachine(void) {};
-void LedStateMachine(void) {};
-void _LedStateMachine(void) {};
